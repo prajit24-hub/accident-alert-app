9a0de26ae4a19249a4620725063f9b1104373b76
prajit24-hub <kit25csecprajit.va@gmail.com>
2026-01-29 02:25:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,37 @@
-let map, marker;
+function autoSendSOS() {
 
-function startTracking() {
-  const status = document.getElementById("status");
-  status.innerText = "Getting your location...";
-
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        const lat = position.coords.latitude;
-        const lng = position.coords.longitude;
-
-        status.innerText = `Latitude: ${lat}\nLongitude: ${lng}`;
-
-        // Initialize map
-        if (!map) {
-          map = L.map('map').setView([lat, lng], 15);
-          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19
-          }).addTo(map);
-        }
-
-        // Add marker
-        if (marker) {
-          marker.setLatLng([lat, lng]);
-        } else {
-          marker = L.marker([lat, lng]).addTo(map);
-        }
-
-      },
-      error => {
-        status.innerText = "Location permission denied or error!";
-      }
-    );
-  } else {
-    status.innerText = "Geolocation not supported";
+  if (!navigator.geolocation) {
+    alert("GPS not supported");
+    return;
   }
+
+  navigator.geolocation.getCurrentPosition(position => {
+
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const time = new Date().toLocaleString();
+
+    const formURL = "https://docs.google.com/forms/d/e/1FAIpQLSd75j7BXWhMReBrYrzn88mhTtIYGukzjFO12d--HEIX-eXZgA/viewform?usp=publish-editor";
+
+    const data = new FormData();
+    data.append("entry.20476386660", "Test User");                 
+    data.append("entry.1951263741", "friend@gmail.com");           
+    data.append("entry.1106677624", "hospital@gmail.com");         
+    data.append("entry.1111959112", "police@gmail.com");           
+    data.append("entry.1743056932", lat);                          
+    data.append("entry.941773419", lng);                           
+    data.append("entry.354965029", time);                          
+
+    fetch(formURL, {
+      method: "POST",
+      mode: "no-cors",
+      body: data
+    });
+
+    document.getElementById("status").innerText =
+      "🚨 SOS SENT AUTOMATICALLY\nLocation shared";
+
+  }, () => {
+    alert("Location permission denied");
+  });
 }
