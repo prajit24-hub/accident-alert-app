ee078a87d92e86eeea1362de9f10412152b296b1
prajit24-hub <kit25csecprajit.va@gmail.com>
2026-01-29 07:53:00 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,2 @@
-self.addEventListener("install", () => {
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", () => {
-  self.clients.claim();
-});
+self.addEventListener('install', (e) => { console.log('Service Worker: Installed'); });
+self.addEventListener('fetch', (e) => { e.respondWith(fetch(e.request)); });
